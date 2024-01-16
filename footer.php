0f7e2041340da884e213b3c56c2edac49005bce3
wolfpac <cbellec@normandiewebschool.fr>
2024-01-16 11:27:08 +0100

    training greenit

---
+++ footer.php
@@ -0,0 +1,6 @@
+</main>
+    <footer class="bg-black text-slate-500 text-center py-4 mt-auto">
+        <p>&copy; <?php echo date("Y"); ?> UnderCity. Tous droits réservés.</p>
+    </footer>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,40 @@
+<?php
+include_once 'header.php';
+?>
+<link rel="stylesheet" href="styles.css">
+<section id="hero" class="bg-slate-500">
+    <div class="p-8">
+        <h1 class="text-white text-3xl font-bold">UnderCity</h1>
+    </div>
+    <div class="p-8 ">
+        <h2 class="text-white text-2xl font-bold mb-10">Lorem ipsum.</h2>
+        <p class="text-white text-xl mb-10">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum error beatae quibusdam sit odit dolores laboriosam quo minima. Fugit ad dicta praesentium eum ipsa similique aperiam deleniti placeat laborum perferendis!</p>
+    </div>
+</section>
+<section id="bg-YT" class="p-8">
+    <div class="block">
+        <div class="flex items-center justify-center">
+            <a class="w-1/4 h-1/8" href="https://www.youtube.com/embed/eogkyCnQQzM?list=TLGGGjjnOcqPZkExNjAxMjAyNA" target="_blank" rel="noopener noreferrer">
+                <img class=" h-1/4" src="https://img.youtube.com/vi/eogkyCnQQzM/maxresdefault.jpg" alt="Music makes you concentrate on study in the evening">
+            </a>
+        </div>
+    </div>
+</section>
+<section id="parternaire" class="bg-slate-800">
+<div class="p-8">
+        <h2 class="text-white text-center text-2xl font-bold">Nos partenaires</h2>
+    </div>
+    <div class="p-8">
+        <p class="text-white text-center text-xl mb-10">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum error beatae quibusdam sit odit dolores laboriosam quo minima. Fugit ad dicta praesentium eum ipsa similique aperiam deleniti placeat laborum perferendis!</p>
+        <div class="flex justify-center items-center">
+            <img class="w-1/3" src="./assets/64063.jpg" alt="Logos partenaires">
+        </div>
+        
+    </div>
+
+</section>
+
+
+<?php
+include_once 'footer.php';
+?>
